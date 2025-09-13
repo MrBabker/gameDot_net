99b8f223cc5d59a603889d246285be8f861a42c7
MrBabker <smilemedo@outlook.com>
2025-09-13 19:44:36 +0400

    first commit

---
+++ APIs/Controllers/PlayersInfo.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Supabase;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System.Numerics;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersInfo : ControllerBase
+    {
+        
+        private readonly Client _supabase;
+
+       public PlayersInfo(Client supabase)
+        {
+            _supabase = supabase;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Players>>> GetAll()
+        {
+            var result = await _supabase.From<Players>().Get();
+
+            if (result.Models == null || result.Models.Count == 0)
+                return NotFound();
+
+            return result.Models; 
+        }
+    }
+}
+
+public class Players : BaseModel
+{
+    [PrimaryKey("id", false)]
+  
+    public string email { get; set; }
+  
+    public string name { get; set; }
+
+    public string created_at { get; set; }
+    public string updated_at { get; set; }
+}
---
+++ APIs/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Supabase;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var url = builder.Configuration["Supabase:Url"];
+var key = builder.Configuration["Supabase:Key"];
+// تسجيل Supabase Client في DI
+builder.Services.AddSingleton(provider =>
+{
+    var client = new Client(url, key);
+    client.InitializeAsync().Wait(); // مهم تهيئه
+    return client;
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
